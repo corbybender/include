f057d5976ed6c506f1cc3b72a022f2b3d531742f
Corby Bender <corby.bender@valmont.com>
2023-05-05 11:16:39 -0500

    Add files via upload

---
+++ valmontcom.js
@@ -0,0 +1,70 @@
+
+//********************************* */
+// Global site tag (gtag.js) -  Google Universal Analytics
+//********************************* */
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-5083046-33"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-5083046-33');
+</script>
+//********************************* */
+//End Global site tag (gtag.js) -  Google Universal Analytics
+//*************************************************************************************************** */
+
+
+
+//********************************* */
+// Global site tag (gtag.js) -  Google Analytics GA4
+//********************************* */
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-G4CBRMPC85"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-G4CBRMPC85');
+</script>
+//********************************* */
+//End Global site tag (gtag.js) -  Google Analytics GA4
+//*************************************************************************************************** */
+
+
+
+//********************************* */
+// Google Tag Manager for Valmont.com
+//********************************* */
+<script type="text/javascript">
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T8LJFN6');
+</script>
+//********************************* */
+//End Google Tag Manager for Valmont.com
+//*************************************************************************************************** */
+
+
+
+//********************************* */
+// CrazyEgg Analytics for valmont.com
+<script type="text/javascript" src="//script.crazyegg.com/pages/scripts/0087/4975.js" async="async" >
+</script>
+//********************************* */
+//End CrazyEgg Analytics for valmont.com
+//*************************************************************************************************** */
+
+
+
+//********************************* */
+// OneTrust Cookies Consent Notice start for valmont.com
+//********************************* */
+<script type="text/javascript" src="https://cdn.cookielaw.org/consent/70fb6e45-bc93-4f4c-832b-5e4052c335dc/OtAutoBlock.js" ></script>
+<script src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"  type="text/javascript" charset="UTF-8" data-domain-script="70fb6e45-bc93-4f4c-832b-5e4052c335dc" ></script>
+<script type="text/javascript">
+function OptanonWrapper() { }
+</script>
+//********************************* */
+// End OneTrust Cookies Consent Notice start for valmont.com
+//*************************************************************************************************** */
